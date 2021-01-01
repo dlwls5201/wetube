b678507ccb5a90f4f2aafdb1983e410733f25bdf
dlwls5201 <dlwls5201@naver.com>
2021-01-01 22:05:01 +0900

    유튜브 클론2차 작업

--- app.js
+++ app.js
@@ -6,17 +6,19 @@ import bodyParser from 'body-parser';
 import globalRouter from './routes/globalRouter';
 import userRouter from './routes/userRouter';
 import videoRouter from './routes/videoRouter';
+import routes from './routes';
 
-const app = express();
+const app= express();
 
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
