819440fcd7d3015808f0a06570dec504bca1f408
dlwls5201 <dlwls5201@naver.com>
2021-01-02 16:13:02 +0900

    3일차 작업

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import { localMiddleware } from './middlewares';
+import routes from './routes';
 import globalRouter from './routes/globalRouter';
 import userRouter from './routes/userRouter';
 import videoRouter from './routes/videoRouter';
-import routes from './routes';
 
 const app= express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localMiddleware)
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-export const join = (req, res) => res.render("join");
+export const join = (req, res) => res.render("join", { pageTItle : "Join" });
 export const login = (req, res) => res.render("login");
 export const logout = (req, res) => res.render("logout");
 export const users = (req, res) => res.render("users");
