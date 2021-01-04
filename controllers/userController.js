9db63c7193e52e5d13fe5bb9830286751cb1196b
dlwls5201 <dlwls5201@naver.com>
2021-01-04 23:35:05 +0900

    html 기본 작업 완료

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,37 @@
-export const join = (req, res) => res.render("join", { pageTitle : "Join" });
+import routes from '../routes';
 
-export const login = (req, res) => res.render("login", { pageTitle : "Login" });
+export const getJoin = (req, res) => {
+    res.render("join", { pageTitle : "Join" })
+}
 
-export const logout = (req, res) => res.render("logout", { pageTitle : "Logout" });
+export  const postJoin = (req, res) => {
+    console.log(req);
+    const { 
+        body:  { name, email, password, password2 }
+    } = req;
+
+    if(password !== password2) {
+        res.status(400);
+        res.render("join", { pageTitle : "Join" });
+    } else {
+        // To Do: Register User
+        // To Do: Log user in
+        res.redirect(routes.home);
+    }
+}
+
+export const getLogin = (req, res) => {
+    res.render("login", { pageTitle : "Login" });
+}
+
+export const postLogin = (req, res) => {
+    res.redirect(routes.home)
+}
+
+export const logout = (req, res) => {
+    // To Do: Process Log Out
+    res.redirect(routes.home);
+}
 
 export const users = (req, res) => res.render("users", { pageTitle : "Users" });
 
